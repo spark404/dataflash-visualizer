78633411788ffab7ee1c6296decbdee4be5f9d71
Hugo Trippaers <hugo@trippaers.nl>
2017-10-25 21:53:55 +0200

    Minor cleanup of a lambda

--- aws/dataflash-listDataflashlogs/handler.js
+++ aws/dataflash-listDataflashlogs/handler.js
@@ -1,57 +1,57 @@
-'use strict';
+"use strict";
 
 exports.handler = function(event, context, callback) {
-	const AWS = require("aws-sdk");
-	const Q = require('kew');
-	const docClient = new AWS.DynamoDB.DocumentClient();
+    const AWS = require("aws-sdk");
+    const Q = require("kew");
+    const docClient = new AWS.DynamoDB.DocumentClient();
 
-	function queryDatabase() {
-	    console.log("Entering queryDatabase")
-		const defer = Q.defer();
+    function queryDatabase() {
+        console.log("Entering queryDatabase");
+        const defer = Q.defer();
 
-		var params = {
-	    	TableName : "dataflashlogheaders",
-	    	AttributesToGet : [
-	    		"filename",
-	    		"altitude",
-	    		"attitude",
-	    		"err",
-	    		"gps",
-	    		"imu",
-	    		"ntun",
-	    		"power",
-	    		"size",
-	    		"timestamps"
-	    	]
-		};
+        var params = {
+            TableName : "dataflashlogheaders",
+            AttributesToGet : [
+                "filename",
+                "altitude",
+                "attitude",
+                "err",
+                "gps",
+                "imu",
+                "ntun",
+                "power",
+                "size",
+                "timestamps"
+            ]
+        };
 
-		docClient.scan(params, function(err, data) {
-		    if (err) {
-		        defer.reject(err)
-		    } else {
-		        defer.resolve(data.Items)
-		    }
-		});
+        docClient.scan(params, function(err, data) {
+            if (err) {
+                defer.reject(err);
+            } else {
+                defer.resolve(data.Items);
+            }
+        });
 
-		return defer.promise
-	}
+        return defer.promise;
+    }
 
-	function response(data, statuscode) {
-	    console.log("Entering response")
+    function response(data, statuscode) {
+        console.log("Entering response");
 
-	    const response = {
-    		statusCode: statuscode,
-    		headers: {
-    		    "Access-Control-Allow-Origin": "*"
-    		},
-		    body: JSON.stringify(data),
-		  };
-		
-		callback(null, response)
-	} 
+        const response = {
+            statusCode: statuscode,
+            headers: {
+                "Access-Control-Allow-Origin": "*"
+            },
+            body: JSON.stringify(data)
+        };
 
-    console.log("*** MAIN ***")
-	queryDatabase()
-		.then(data => response(data, 200))
-		.fail(err => response(err, 500))
-}
+        callback(null, response);
+    }
+
+    console.log("*** MAIN ***");
+    queryDatabase()
+        .then(data => response(data, 200))
+        .fail(err => response(err, 500));
+};

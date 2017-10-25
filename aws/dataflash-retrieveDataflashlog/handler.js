e860d31b6559a61005a3e15de31cf09b091a9b45
Hugo Trippaers <hugo@trippaers.nl>
2017-10-25 22:46:23 +0200

    More cleanups and some parameter checking

--- aws/dataflash-retrieveDataflashlog/handler.js
+++ aws/dataflash-retrieveDataflashlog/handler.js
@@ -1,48 +1,57 @@
-'use strict';
+"use strict";
 
 exports.handler = function(event, context, callback) {
-	const AWS = require("aws-sdk");
-	const Q = require('kew');
-	const docClient = new AWS.DynamoDB.DocumentClient();
-	const util = require('util')
+    const AWS = require("aws-sdk");
+    const Q = require("kew");
+    const docClient = new AWS.DynamoDB.DocumentClient();
+    const util = require("util");
 
-	function queryDatabase() {
-	    console.log("Entering queryDatabase")
-		const defer = Q.defer();
+    if (event === null || !event.hasOwnProperty("resource")) {
+        callback(new Error("Probably not a request from the API gateway"))
+    }
 
-		var params = {
-	    	TableName : "dataflashlogheaders",
-	    	Key: {
-	    		filename: event.pathParameters.reportid
-	    	}
-		};
+    if (!event.pathParameters.hasOwnProperty("reportId") || event.pathParameters['reportId'] === undefined) {
+        response(new Error("Missing required path parameter reportId"), 409)
+    }
 
-		docClient.get(params, function(err, data) {
-		    if (err) {
-		        defer.reject(err)
-		    } else {
-		        defer.resolve(data)
-		    }
-		});
+    function queryDatabase() {
+        console.log("Entering queryDatabase");
+        const defer = Q.defer();
 
-		return defer.promise
-	}
+        var params = {
+            TableName : "dataflashlogheaders",
+            Key: {
+                filename: event.pathParameters.reportid
+            }
+        };
 
-	function response(data, statuscode) {
-	    console.log("Entering response")
+        docClient.get(params, function(err, data) {
+            if (err) {
+                defer.reject(err);
+            } else {
+                defer.resolve(data);
+            }
+        });
 
-	    const response = {
-    		statusCode: statuscode,
-		    body: JSON.stringify(data),
-		  };
-		
-		callback(null, response)
-	} 
+        return defer.promise;
+    }
 
-    console.log("*** MAIN ***")
-    console.log("event: " + util.inspect(event))
-    console.log("context: " + util.inspect(context))
-	queryDatabase()
-		.then(data => response(data, 200))
-		.fail(err => response(err, 500))
-}
+    function response(data, statuscode) {
+        console.log("Entering response");
+
+        const response = {
+            statusCode: statuscode,
+            body: JSON.stringify(data)
+
+        };
+
+        callback(null, response);
+    }
+
+    console.log("*** MAIN ***");
+    console.log("event: " + util.inspect(event));
+    console.log("context: " + util.inspect(context));
+    queryDatabase()
+        .then(data => response(data, 200))
+        .fail(err => response(err, 500));
+};
